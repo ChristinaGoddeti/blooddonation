04b74b87142b760a9dcdffd1610301fbed4ac0c7
ChristinaGoddeti <155823322+ChristinaGoddeti@users.noreply.github.com>
2024-03-10 17:55:03 +0530

    added about.php

---
+++ about.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<style>
+body{background-color:red;}
+h3{color:white;}
+p{color:white;}
+</style>
+</head>
+<body><center>
+<h3> A PROJECT BASED BLOOD DONATION MANAGEMENT SYSTEM.</h3>
+<p>Blood Donation is a voluntary procedure that can help save lives.<br>
+There are several types of blood donation.Each type helps meet different medical needs.
+Our BDMS is just a project based website but looking unto our website we feel like launching it soon in future once it gets 
+a better way.<br>
+The more about our website we can say is if someone is really in need of blood our website is 24 hours available by providing you
+the reference link of Blood Campus,Clinic and Hospitals.<br>
+For that we have provided some links</p>
+REFERENCE LINKS ARE GIVEN BELOW:<br>
+ <a href="https://eraktkosh.in/BLDAHIMS/bloodbank/transactions/bbpublicindex.html">
+ https://eraktkosh.in/BLDAHIMS/bloodbank/transactions/bbpublicindex.html</a><br>
+ <a href="https://www.redcrossblood.org/account-login.html">
+ https://www.redcrossblood.org/account-login.html</a><br>
+ <a href="https://www.friends2support.org/">
+ https://www.friends2support.org/</a><br>
+ <a href="https://www.blood.co.uk/the-donation-process/registering-online/">
+ https://www.blood.co.uk/the-donation-process/registering-online/</a><br>
+ <a href="https://www.oneblood.org/">
+ https://www.oneblood.org/</a><br>
+ <p>Please go and visit and be the resaon of someones life.</p></center>
+ <center><a href="#">FOR MORE DETAILS</a></center>
+ <center>
+ CONTACT US:<br>
+ Ms.Christina Goddeti   8956493177<br>
+ Ms.Vrushali Pachghare  9689861712<br>
+ Ms.Shrutika Mane       8459378851<br>
+ </center>
+ <center>
+ <img src="C:\Users\godde\Downloads\blood-donation.jpg" width=300 height=300>
+ </center>
+</body>
+</html>
